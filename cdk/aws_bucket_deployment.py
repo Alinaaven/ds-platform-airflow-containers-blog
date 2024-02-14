acc483ca8ad56a12b45c8d2f299b840248399563
Alinaaven <43969679+Alinaaven@users.noreply.github.com>
2024-02-14 11:05:03 +0200

    Create aws_bucket_deployment.py

---
+++ cdk/aws_bucket_deployment.py
@@ -0,0 +1,34 @@
+from aws_cdk import core
+from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_iam as iam
+
+class MyEcsClusterStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Create a new VPC
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=3)
+
+        # Create the ECS Cluster
+        cluster = ecs.Cluster(self, "MyCluster", vpc=vpc)
+
+        # Create an ECS Task Role
+        task_role = iam.Role(self, "ecsTaskRole",                            assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),                             description="Role that the ECS task can assume")
+        task_role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonECSTaskExecutionRolePolicy"))
+        task_role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2ContainerServiceforEC2Role"))
+
+          # Create an ECS Task Execution Role
+        execution_role = iam.Role(self, "ecsExecutionRole",                             assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),                                   description="Task Execution Role that ECS can assume")
+        execution_role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2ContainerRegistryReadOnly"))
+        execution_role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchLogsFullAccess"))
+        execution_role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonECSTaskExecutionRolePolicy"))
+        execution_role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2ContainerServiceforEC2Role"))
+
+
+        # Additional configurations can be added here
+
+app = core.App()
+MyEcsClusterStack(app, "MyEcsClusterStack")
+app.synth()
